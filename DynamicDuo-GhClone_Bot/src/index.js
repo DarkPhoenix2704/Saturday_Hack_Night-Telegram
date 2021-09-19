e83f98e9a2687d7aa579285d89fbe61a8320682d
Anbarasu <anbarasun123@gmail.com>
2021-09-19 08:18:44 +0530

    Bot replies to hello🙂

--- DynamicDuo-GhClone_Bot/src/index.js
+++ DynamicDuo-GhClone_Bot/src/index.js
@@ -1,26 +1,45 @@
 require('dotenv')
 const express = require('express')
+const axios = require("axios");
 const app = express()
-const axios = require('axios')
-const bodyParser = require("body-parser");
-const {BOT_TOKEN} = process.env
+app.use(express.json())
 
-const TELEGRAM_API = `https://api.telegram.org/bot${BOT_TOKEN}`
-app.use(bodyParser.json())
+const BOT_TOKEN = process.env.BOT_TOKEN
 
-app.post('/', async (req) => {
-    console.log(req.body)
-    if (req.body.hasOwnProperty('my_chat_member')) {
-        console.log('Group Chat Not Allowed')
-    } else {
-        const chatId = req.body.message.chat.id
-        const text = req.body.message.text
+const TELEGRAM_API = 'https://api.telegram.org/bot' + BOT_TOKEN
+console.log(TELEGRAM_API)
 
+app.post('/', (req,res) => {
+    if (req.body.hasOwnProperty('my_chat_member')) { console.log('Group Message'); return; }
+    if (req.body.message || req.body.channel_post) {
+        console.log(req.body)
+        console.log('It\'s a dm')
+
+        const chatId = req.body.message.chat.id;
+        const sentMessage = req.body.message.text;
+        if (sentMessage === 'hello') {
+            console.log('Sending hELLO')
+            axios.post(TELEGRAM_API + '/sendMessage',
+                {
+                    chat_id: chatId,
+                    text: 'hello back 👋'
+                })
+                .then((response) => {
+                    console.log(response)
+                    res.status(200).send(response);
+                }).catch((error) => {
+                    console.log(error)
+                res.send(error);
+            });
+        } else {
+            // if no hello present, just respond with 200
+            res.status(200).send({});
+        }
     }
-
 
 })
 
-app.listen(80, async () => {
+
+app.listen(80, ()=> {
     console.log('🚀 app running on port', 80)
 })
